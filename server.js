3cccaa59825857776c18e265ccfbcff5e5522458
bzrk <abngu2@student.monash.edu>
2018-05-14 13:54:49 +1000

    Working on upload function

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var faceDetection = require("./FaceDetection.js");
 var textToSpeech = require("./TexttoSpeech.js");
 var fs = require("fs");
+var SocketIOFile = require('socket.io-file');
 var app = require("express")(); //initialise express app
 var server = require('http').Server(app); //create server
 var io = require("socket.io")(server); //create socket
-
+var formidable = require("formidable");
 //initalise server to listen on port 3000
 server.listen(3000, function(){
   console.log("Server listening on port 3000");
@@ -12,11 +13,50 @@ server.listen(3000, function(){
 
 //send index.html
 app.get('/', function(req, res){
-  res.sendfile(__dirname + "/index.html");
-  console.log(req);
+  res.sendFile(__dirname + "/index.html");
+  console.log("Accessing index page");
 });
+
+//Upload file
+/*app.post('/fileupload', function(req, res){
+  console.log("Uploading file");
+  var form = new formidable.IncomingForm();
+  form.parse(req, function(err, fields, files){
+    console.log(files.img.path);
+    var oldPath = files.img.path;
+    var newPath = __dirname + '/files/' + files.img.name;
+    fs.rename(oldPath, newPath, function(err){
+      if (err) throw err;
+      console.log("Upload complete");
+
+      res.send("File upload complete");
+    })
+  })
+})*/
 
 //listen for connection
 io.on('connection', function(socket){
   console.log("Client " + socket.id + " has connected");
+  var uploader = new SocketIOFile(socket, {
+    uploadDir: './files',
+    transmissionDelay: 0,
+    overwrite: true
+  });
+  uploader.on('start', (fileInfo) => {
+    console.log("Client started uploading file");
+    console.log(fileInfo);
+  })
+  uploader.on('stream', (fileInfo) => {
+    var percent = fileInfo.wrote/fileInfo.size * 100;
+    console.log(percent + "% completed");
+    console.log(fileInfo);
+  })
+  uploader.on('complete', (fileInfo) => {
+    console.log("Transmission complete");
+    console.log(event);
+  })
+  uploader.on('error', (err) => {
+    console.log("Error in upload");
+    console.log(err);
+  })
 });
